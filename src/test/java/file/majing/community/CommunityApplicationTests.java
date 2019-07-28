3add235a88254b01b818f2aaaf3ac7bcbb03f058
hcty <1481214393@qq.com>
2019-07-28 19:09:07 +0800

    init repo

---
+++ src/test/java/file/majing/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package file.majing.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class) @SpringBootTest public class CommunityApplicationTests {
+
+	@Test public void contextLoads() {
+	}
+
+}
