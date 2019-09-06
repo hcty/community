e1ebb875e0076ce0ad4320178f9eafb89dfa31e4
hcty <1481214393@qq.com>
2019-09-06 22:44:22 +0800

    实现回复功能-不刷新页面登录

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -14,7 +14,14 @@ function post() {
             if(data.code == 200){
                 $("#comment_section").hide();
             }else{
-                alert(data.message);
+                if(data.code == 2003){
+                    if (confirm(data.message)){
+                        window.open("https://github.com/login/oauth/authorize?client_id=dcf0854013085140d0d4&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closeable",true);
+                    }
+                }else{
+                    alert(data.message);
+                }
             }
             console.log(data);
         },
